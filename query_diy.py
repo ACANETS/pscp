45ee83e003323dad3c8181cdef13f35fe2cbd82f
BlueRoy <Roy@Chen.local>
2015-09-29 12:23:10 -0400

    update license

--- query_diy.py
+++ query_diy.py
@@ -1,5 +1,5 @@
 ###########################################################################
-# Copyright 2015 by ACANETS                                               #
+# Copyright 2015 by ACANETS Lab at University of Massachusetts Lowell     #
 #                                                                         #
 # Licensed under the Apache License, Version 2.0 (the "License");         #
 # you may not use this file except in compliance with the License.        #
